2cf4b4dbd31de13f7e1c5b0d3c0f2d5098892110
ElTinmar <martin.privat@hotmail.fr>
2025-03-21 23:59:42 +0100

    update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='geometry',
     author='Martin Privat',
-    version='0.2.16',
+    version='0.2.17',
     packages=['geometry','geometry.tests'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     description='geometry functions',
