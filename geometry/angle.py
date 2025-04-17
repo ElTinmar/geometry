760a5dd1e42b0ee46fad5d1041dcc5b2ca437aad
ElTinmar <martin.privat@hotmail.fr>
2025-04-17 10:56:39 +0200

    add normalize and one liners

--- geometry/angle.py
+++ geometry/angle.py
@@ -14,3 +14,16 @@ def angdiff(a1: float, a2: float) -> float:
     angle_diff = a1 - a2
     angle_diff = ((angle_diff + np.pi) % (2*np.pi)) - np.pi
     return angle_diff
+
+def normalize_angle(theta: float) -> float:
+    '''returns angle in [-pi, pi]'''
+    return np.arctan2(np.sin(theta), np.cos(theta))
+
+'''
+def angular_difference(a, b):
+    #signed angular difference
+    return np.arctan2(np.sin(a - b), np.cos(a - b))
+
+def angle_between_vectors(v1: NDArray, v2: NDArray) -> float:
+    return np.arctan2(np.cross(v1,v2), np.dot(v1,v2))
+'''
